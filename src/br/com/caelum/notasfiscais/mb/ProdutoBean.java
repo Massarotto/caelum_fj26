40e2e9cececb9ae106f10536d6ebfdf3aeaa0b28
Fabio Massarotto <fsmassarotto@gmail.com>
2013-07-20 16:29:27 -0300

    Alterar e remover produto

--- src/br/com/caelum/notasfiscais/mb/ProdutoBean.java
+++ src/br/com/caelum/notasfiscais/mb/ProdutoBean.java
@@ -1,23 +1,52 @@
 package br.com.caelum.notasfiscais.mb;
 
+import java.util.List;
+
 import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
 
 import br.com.caelum.notasfiscais.dao.DAO;
 import br.com.caelum.notasfiscais.modelo.Produto;
 
-@ManagedBean
+@ManagedBean 
+@ViewScoped
 public class ProdutoBean {
 		
 	private Produto produto = new Produto();
 
+	private List<Produto> produtos;
+	
 	public void grava(){
 		DAO<Produto> dao = new DAO<>(Produto.class);
-		dao.adiciona(this.produto);
+		
+		if(produto.getId() == null) {
+			dao.adiciona(produto);
+		}else {
+			dao.atualiza(produto);
+		}
 		this.produto = new Produto();
+		this.produtos = null;
 	}
 	
 	public Produto getProduto() {
 		return produto;
 	}
 	
+	public void setProduto(Produto produto) {
+		this.produto = produto;
+	}
+
+	public List<Produto> getProdutos() {
+		if(produtos == null) {
+			System.out.println("Carregando produtos...");
+			produtos = new DAO<Produto>(Produto.class).listaTodos();
+		}
+		return produtos;
+	}
+	
+	public void remove(Produto produto) {
+		DAO<Produto> dao = new DAO<>(Produto.class);
+		dao.remove(produto);
+		this.produtos = dao.listaTodos();
+	}
 }
