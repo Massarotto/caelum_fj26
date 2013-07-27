a6c22de1b7f4a466ff8223685bc88a313d4203d5
Fabio Massarotto <fsmassarotto@gmail.com>
2013-07-27 14:56:09 -0300

    Adicionando tela de login

---
+++ src/br/com/caelum/notasfiscais/mb/LoginBean.java
@@ -0,0 +1,32 @@
+package br.com.caelum.notasfiscais.mb;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+import br.com.caelum.notasfiscais.dao.UsuarioDAO;
+import br.com.caelum.notasfiscais.modelo.Usuario;
+
+@ManagedBean
+@SessionScoped
+public class LoginBean {
+
+	private Usuario usuario = new Usuario();
+	
+	public String efetuaLogin() {
+		UsuarioDAO dao = new UsuarioDAO();
+		if(dao.existe(usuario)) {
+			return "produto?faces-redirect=true";
+		}
+		return "login?faces-redirect=true";
+	}
+	
+	public String efetuaLogout() {
+		this.usuario = new Usuario();
+		return "login?faces-redirect=true";
+	}
+	
+	public Usuario getUsuario() {
+		return usuario;
+	}
+	
+}
